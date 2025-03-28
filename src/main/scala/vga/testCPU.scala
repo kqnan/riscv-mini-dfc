b16abeb70b04a61035fc5dd6e30e52e93e5d30d1
LM10essi <741260264@qq.com>
2025-03-28 22:16:32 +0800

    读写测试中

--- src/main/scala/vga/testCPU.scala
+++ src/main/scala/vga/testCPU.scala
@@ -11,24 +11,29 @@ class testCPU extends Module{
     val vga_rgb=Output(UInt(12.W))
   })
   val vga=Module(new vga_colorbar)
-//  val regmapper=Module(new RegMapper()((new MiniConfig).toInstance))
-//  val selector = Module(new Selector()((new MiniConfig).toInstance))
-//  val core= Module(new Core(1,0x200,(new MiniConfig).toInstance))
-//  io.hsync:=vga_colorbar.io.hsync
-//  io.vsync:=vga_colorbar.io.vsync
-//  io.vga_rgb:=vga_colorbar.io.vga_rgb
-//
-//  regmapper.io.vga<>vga_colorbar.io.cpu
-//  regmapper.io.selector<>selector.io.devices
-//  regmapper.io.uart:=DontCare
-//  regmapper.io.rtc:=DontCare
-//  selector.io.dcache:=DontCare
-//  selector.io.cpu<>core.io.dcache
-//
+  val x=withReset(!reset.asBool()){RegInit(0.U(10.W))}
+  val y=withReset(!reset.asBool()){RegInit(0.U(9.W))}
 
-  val base_addr=RegInit("b00011000000000000000000000000000".U)
-                        //"b0001100000000000????????????????"  //????????????????"
+  val base_addr="b0001100000000000".U##x##y
+  vga.io.cpu.req.bits.mask:=0XFFF.U(12.W)
+  vga.io.cpu.req.bits.addr:=base_addr
+  vga.io.cpu.req.bits.data:=x
+  io.hsync:=vga.io.hsync
+  io.vsync:=vga.io.vsync
+  io.vga_rgb:=vga.io.vga_rgb
+  when(x===639.U){
+    y:=0.U
+    when(y===479.U){
+      y:=0.U
+    }.otherwise{
+      y:=y+1.U
+    }
+  }.otherwise{
+    x:=x+1.U
+  }
+  vga.io.cpu.req.valid:=true.B
   vga.io.cpu.abort:=false.B
-
-
+}
+object emit extends App{
+  (new chisel3.stage.ChiselStage).emitVerilog(new testCPU())
 }
